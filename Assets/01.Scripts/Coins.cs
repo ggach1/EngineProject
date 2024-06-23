d5d63387865b80db6e700590024aa9e28a0fb82c
ggach1 <dlsdnj1234@gmail.com>
2024-06-23 14:52:48 +0900

    Finish CoinSettting

--- Assets/01.Scripts/Coins.cs
+++ Assets/01.Scripts/Coins.cs
@@ -4,10 +4,23 @@ using UnityEngine;
 
 public class Coins : MonoBehaviour
 {
+    private ScoreManager _scoreManager;
+
+    private int _currentScore = 0;
+
+    private void Awake()
+    {
+        _scoreManager = FindObjectOfType<ScoreManager>();
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
+            _currentScore += 1;
+
+            _scoreManager.Setscore(_currentScore);
+            
             Destroy(gameObject);
 
             //Debug.Log("코인 얻음");
